f1371c988239b9a4102733fba32a6ad3ad9355e8
Leonardo Bezerra <leosoares.bezerra2017@gmail.com>
2023-10-18 19:11:34 -0300

    Transcrevendo em linha de comando

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import os, sys
 from videotools.converter import *
+import videotools.chunks as chunks
+from videotools.transcribe import *
+import os
+import moviepy.editor as mp
+from pydub import AudioSegment
+from pydub.utils import make_chunks
 
 def main(args):
     #convertendo de mp4 para mp3
     videoMp4=args[1]
+    destino=args[2]
 
-    converter(videoMp4,"mp4","mp3")
+    
+
+    #mp4_to_mp3(videoMp4,destino)
+    converter(videoMp4,"mp4","mp3", os.path.relpath(destino))
+
+    #mp3_to_wav(destino,destino)
+    converter(os.path.join(destino,"teste.mp3"),"mp3","wav", os.path.relpath(destino))
+
+    #criando chunks
+    chunks.timechunk = 30000
+    chunks.makeChunks(os.path.join(destino,"teste.wav"),os.path.join(destino,"chunks"))
+
+    #transcrevendo
+
+    with open(os.path.join(destino,"transcricao.txt"),"w") as f:
+        f.write(transcreve_audio(os.path.join(destino,"teste.wav"),forGPT=True))
 
     ...
 
